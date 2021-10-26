2436d46a16c340a612854995688cddc661df478e
yida-li <gabrielyidali@example.com>
2021-10-26 16:19:57 -0400

    added actual recipes

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import DrinkBox from './DrinkBox'
-import pimage1 from '../images/drink1.png'
-import pimage2 from '../images/drink2.png'
+import pimage1 from '../images/toyko.png'
+
 
 export default function Drinks() {
     return (
@@ -9,12 +9,12 @@ export default function Drinks() {
             <div className="App"><h1>Choose a Drink</h1></div>
             
         <div className='a-container'>
-        <DrinkBox image={pimage1} title="CrayCray"/>
-        <DrinkBox image={pimage2} title="walmart tomato juice"/>
-        <DrinkBox image={pimage2} title="walmart tomato juice"/>
-        <DrinkBox image={pimage2} title="walmart tomato juice"/>
-        <DrinkBox image={pimage2} title="walmart tomato juice"/>
-        <DrinkBox image={pimage2} title="walmart tomato juice"/>
+        <DrinkBox image={pimage1} title="Tokyo Cocktail"/>
+        <DrinkBox image={pimage1} title="Tokyo Cocktail"/>
+        <DrinkBox image={pimage1} title="Tokyo Cocktail"/>
+        <DrinkBox image={pimage1} title="Tokyo Cocktail"/>
+        <DrinkBox image={pimage1} title="Tokyo Cocktail"/>
+        <DrinkBox image={pimage1} title="Tokyo Cocktail"/>
         </div>
 
         </div>
